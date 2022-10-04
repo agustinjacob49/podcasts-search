8d6a5f6568627103206cfa86d4ab48749d9091cf
Agustin Jacobsen <jacobsen816@gmail.com>
2022-10-04 17:42:30 -0300

    routes episodes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
+import Home from './pages/home/view';
+import EpisodeDetails from './pages/episode-detail/view';
+import PodcastDetails from './pages/podcast-details/view';
 
 const App = () =>{
   return (
     <div className="App">
       <Navbar />
+      <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="podcast/:podcastId" element={<PodcastDetails />} />
+                <Route path="podcast/:podcastId/episode/:episodeId" element={<EpisodeDetails />} />
+              </Routes>
+      </Router>
     </div>
   );
 }
