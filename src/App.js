b1ed68b4ce4a163d2bc258a003e4760ce9154f01
Agustin Jacobsen <jacobsen816@gmail.com>
2022-10-05 10:53:51 -0300

    transform podcasts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EpisodeDetails from "./pages/episode-detail/view";
 import PodcastDetails from "./pages/podcast-details/view";
 import LoaderContext from "./utils/context/loaderContext";
 import PodcastsContext from './utils/context/podcastsContext';
+import { transformPodcasts } from './utils/transform/podcasts';
 
 const App = () => {
 
@@ -15,10 +16,14 @@ const App = () => {
   const [podcastData, setPodcastsData] = useState({});
 
   useEffect(() => {
-    setTimeout( () => {
-      setPodcastsData({ title: 'hola' }); 
-      setIsLoading(false);
-    }, 3500);
+    fetch("https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json")
+      .then((response) => response.json())
+      .then((response) => {
+        const { feed : { entry } } = response;
+        const podcasts = transformPodcasts(entry);
+        setPodcastsData({ podcasts }); 
+        setIsLoading(false);
+      });
   }, []);
 
   return (
