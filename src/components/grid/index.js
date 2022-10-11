a06be5ef064cc7c4d725f2a9e7e599cde49725ee
Agustin Jacobsen <jacobsen816@gmail.com>
2022-10-11 09:45:20 -0300

    componentization and parser replaced

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -7,7 +7,7 @@ const Grid = ({ podcasts }) => {
     return (
         <section className='grid'>
             <div className='grid__block'>
-            { podcasts && podcasts.map( p => ( <Item title={p.title} author={p.author} id={p.id} cover={p.cover}/>))}
+            { podcasts && podcasts.map( p => ( <Item title={p.title} author={p.author} id={p.id} cover={p.cover} key={`${p.id}-${p.author}`}/>))}
             </div>
         </section>
     );
